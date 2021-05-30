2f4e9be721fec2eda139e98dd7a35cbae043073e
benesvoj <67459718+benesvoj@users.noreply.github.com>
2021-05-30 11:25:28 +0200

    added routing to leagues, members and clubs

---
+++ src/components/Content/Club/clubsOverview.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import ClubsListHeader from "./clubsListHeader";
+import ClubsListBody from "./clubsListBody";
+
+
+function ClubsOverview() {
+    return (
+        <div>
+            <ClubsListHeader />
+            <ClubsListBody />
+        </div>
+    )
+}
+
+export default ClubsOverview;
